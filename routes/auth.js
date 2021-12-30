9316588d11026002df291c063c5c7bc8cce79ecc
khoa1232123 <38268470+khoa1232123@users.noreply.github.com>
2021-12-30 10:32:30 +0700

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -34,6 +34,7 @@ router.post('/register', async (req, res) => {
 
 // Login
 router.post('/login', async (req, res) => {
+  console.log(req.body);
   try {
     const user = await User.findOne({ email: req.body.email });
     !user && res.status(401).json('Wrong password or username!');
